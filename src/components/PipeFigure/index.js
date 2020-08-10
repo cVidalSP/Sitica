6614afe7c2148e01e5f5fad49dca016deda50aa3
cVidalSP <kainan.vidal@gmail.com>
2020-08-10 15:57:03 -0300

    Adjusting CSS, and design features

---
+++ src/components/PipeFigure/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './index.css';
+
+export default function PipeFigure(){
+    return(
+        <>
+            <div className="pipe1">
+
+            </div>
+            <div className="pipe2">
+
+            </div>
+            <div className="pipe3">
+
+            </div>
+            <div className="pipe4">
+
+            </div>
+            <div className="pipe5">
+
+            </div>
+            <div className="pipe6">
+
+            </div>
+            <div className="pipe7">
+
+            </div>
+            <div className="pipe8">
+
+            </div>
+            <div className="pipe9">
+
+            </div>
+            <div className="pipe10">
+
+            </div>
+            <div className="pipe11">
+
+            </div>
+            <div className="pipe12">
+
+            </div>
+            <div className="pipe13">
+
+            </div>
+            <div className="pipe14">
+
+            </div>
+        </>
+    )
+}
--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './index.css';
+import PipeFigure from '../PipeFigure';
 
 export default function Square(){
     return(
-        <div className="mainSquare">
-            
-        </div>
+        <>
+            <div className="mainSquare">
+                
+            </div>
+            <PipeFigure/>
+        </>
     )
 }
